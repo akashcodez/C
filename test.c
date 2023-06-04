1599d8b5e721122b5f42a3586cdca35288ca4227
Akash raj <akashr.cm.22@nitj.ac.in>
2023-06-04 22:39:29 +0530

    second commit

--- test.c
+++ test.c
@@ -8,6 +8,6 @@ int main(){
     //pi = 3.2; this can't be done as pi is constant and it has been defined by using keyword "define"
     //b=7.22 ;//this cant be done as b is constant in previous line
     printf("Helloworld\n");
-    printf("%f",pi);
+    printf("%f",pi );
     
 }
